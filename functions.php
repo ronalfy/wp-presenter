af2f9d640bdebfecdb25d1dd8c54e3ee207bec45
Trisha Salas <trisha@trishasalas.com>
2015-05-27 10:38:31 -0500

    fix logic for default theme

--- functions.php
+++ functions.php
@@ -22,23 +22,22 @@ add_action( 'after_setup_theme', 'wp_presenter_setup' );
 
 function wp_presenter_scripts() {
 
-	if ( ! is_admin() ) :
 	wp_enqueue_script( 'wp-presenter-head-js', get_template_directory_uri() . '/assets/reveal/lib/js/head.min.js', array(), '', true );
 	wp_enqueue_script( 'wp-presenter-core-js', get_template_directory_uri(). '/assets/reveal/js/reveal.js', array(), '', true );
 	wp_enqueue_script( 'html5shiv', '//cdn.jsdelivr.net/html5shiv/3.7.2/html5shiv.js', array(), '3.7.2', false );
+	wp_enqueue_style( 'wp-presenter-style', get_stylesheet_uri() );
+	wp_enqueue_style( 'wp-presenter-core', get_template_directory_uri() . '/assets/reveal/css/reveal.css' );
+	wp_enqueue_style( 'wp-presenter-monokai', get_template_directory_uri() . '/assets/reveal/lib/css/monokai.css' );
 
-		$theme = esc_html( get_theme_mod( 'select_theme') );
+	$theme = get_theme_mod( 'select_theme' );
+	if ( $theme ) :
+		wp_enqueue_style( 'wp-presenter-display-theme', get_template_directory_uri() . '/assets/reveal/css/theme/' . $theme . '.css' );
+	else :
+		wp_enqueue_style( 'wp-presenter-default-theme', get_template_directory_uri() . '/assets/reveal/css/theme/sky.css' );
+endif;
 
-		wp_enqueue_style( 'wp-presenter-style', get_stylesheet_uri() );
-		wp_enqueue_style( 'wp-presenter-core', get_template_directory_uri() . '/assets/reveal/css/reveal.css' );
 
-		if( $theme ) :
-			wp_enqueue_style( 'wp-presenter-theme', get_template_directory_uri() . '/assets/reveal/css/theme/' . $theme . '.css' );
-		endif;
 
-		wp_enqueue_style( 'wp-presenter-zenburn', get_template_directory_uri() . '/assets/reveal/lib/css/monokai.css' );
-
-	endif;
 }
 	add_action( 'wp_enqueue_scripts', 'wp_presenter_scripts' );
 
@@ -54,7 +53,22 @@ function wp_presenter_load_custom_wp_admin_style() {
 }
 add_action( 'admin_enqueue_scripts', 'wp_presenter_load_custom_wp_admin_style' );
 
-require get_template_directory() . '/inc/acf-fields.php';
+function wp_presenter_acf_settings_path( $path ) {
+	$path = get_template_directory() . '/inc/acf/';
+	return $path;
+}
+add_filter('acf/settings/path', 'wp_presenter_acf_settings_path');
+
+function wp_presenter_acf_settings_dir( $dir ) {
+	$dir = get_template_directory_uri() . '/inc/acf/';
+	return $dir;
+}
+add_filter('acf/settings/dir', 'wp_presenter_acf_settings_dir');
+
+add_filter('acf/settings/show_admin', '__return_false');
+
+define( 'ACF_LITE', true );
+
 require get_template_directory() . '/inc/custom-controls/kirki.php';
 require get_template_directory() . '/inc/template-tags.php';
 require get_template_directory() . '/inc/customizer.php';
@@ -65,3 +79,4 @@ require get_template_directory() . '/inc/dashboard-widgets.php';
 require get_template_directory() . '/inc/acf/acf.php';
 require get_template_directory() . '/inc/acf-image-select/acf-image-select.php';
 require get_template_directory() . '/inc/acf-url-field/acf-website_field.php';
+require get_template_directory() . '/inc/acf-fields.php';
